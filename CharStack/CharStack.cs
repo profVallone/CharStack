7992df674dfd6fe94295079da23271896d73983d
profVallone <profvallone@gmail.com>
2023-10-28 11:53:23 +0200

    Aggiungere i file di progetto.

---
+++ CharStack/CharStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharStack
+{
+    internal class CharStack
+    {
+        private char[] stack;
+        private int index;
+
+        public CharStack(int nElem)
+        {
+            stack = new char[nElem];
+            index = 0;
+        }
+        public bool push(char c)
+        {
+            if (index < stack.Length){
+                stack[index] = c;
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool pop()
+        {
+            if (index > 0)
+            {
+                index--;
+                return true;
+            }
+            return false;
+        }
+        public bool isEmpty()
+        {
+            if (index == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public char getElem()
+        {
+            if (isEmpty()) throw new Exception();
+            return stack[index - 1];
+        }
+    }
+}
---
+++ CharStack/Program.cs
@@ -0,0 +1,53 @@
+﻿using CharStack;
+namespace CharStack;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string s = "(((((("; 
+        Console.WriteLine(checkParentesis(s));
+    }
+    public static bool checkParentesis(string s)
+    {
+        CharStack cs = new CharStack(s.Length);
+        for(int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            try
+            {
+                switch (c)
+                {
+                    case '[':
+                        if (cs.isEmpty() || cs.getElem() != '(')
+                            cs.push(c);
+                        else 
+                            return false;
+                        break;
+                    case '(':
+                        cs.push(c);
+                        break;
+                    case ')':
+                        if (cs.getElem() == '(')
+                            cs.pop();
+                        else
+                            return false;
+                        break;
+                    case ']':
+                        if (cs.getElem() == '[')
+                            cs.pop();
+                        else
+                            return false;
+                        break;
+
+                }
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
+        if (cs.isEmpty())
+            return true;
+        return false;
+    }
+}
